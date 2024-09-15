9a4a8ea54813968d606bbba6e163b2fd474b2419
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-09-15 17:42:31 +0200

    Do not add toll fee if within 1 hour
    pylint

--- Python/TollCalculator.py
+++ Python/TollCalculator.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import holidays
 
 def get_toll_fee_for_entry(vehicle: str, date: datetime):
@@ -21,54 +21,73 @@ def get_toll_fee_for_entry(vehicle: str, date: datetime):
 def get_period_toll_fee(date: datetime):
     period_toll_fee = 0
     if date.hour == 6 and date.minute >= 0 and date.minute <= 29:
-        print(f"[DEBUG] 06:00 => 06:29 tollfee: 8")
+        print("[DEBUG] 06:00 => 06:29 tollfee: 8")
         period_toll_fee += 8
     elif date.hour == 6 and date.minute >= 30 and date.minute <= 59:
-        print(f"[DEBUG] 06:30 => 06:59 tollfee: 13")
+        print("[DEBUG] 06:30 => 06:59 tollfee: 13")
         period_toll_fee += 13
     elif date.hour == 7 and date.minute >= 0 and date.minute <= 59:
-        print(f"[DEBUG] 07:00 => 07:59 tollfee: 18")
+        print("[DEBUG] 07:00 => 07:59 tollfee: 18")
         period_toll_fee += 18
     elif date.hour == 8 and date.minute >= 0 and date.minute <= 29:
-        print(f"[DEBUG] 08:00 => 08:29 tollfee: 13")
+        print("[DEBUG] 08:00 => 08:29 tollfee: 13")
         period_toll_fee += 13
     elif date.hour == 8 and date.minute >= 30 and date.minute <= 59:
-        print(f"[DEBUG] 08:30 => 08:59 tollfee: 8")
+        print("[DEBUG] 08:30 => 08:59 tollfee: 8")
         period_toll_fee += 8
     elif date.hour >=9 and date.hour <= 14:
-        print(f"[DEBUG] 09:00 => 14:59 tollfee: 8")
+        print("[DEBUG] 09:00 => 14:59 tollfee: 8")
         period_toll_fee += 8
     elif date.hour == 15 and date.minute >= 0 and date.minute <= 29:
-        print(f"[DEBUG] 15:00 => 15:29 tollfee: 13")
+        print("[DEBUG] 15:00 => 15:29 tollfee: 13")
         period_toll_fee += 13
     elif date.hour == 15 and date.minute >= 30 and date.minute <= 59:
-        print(f"[DEBUG] 15:30 => 15:59 tollfee: 18")
+        print("[DEBUG] 15:30 => 15:59 tollfee: 18")
         period_toll_fee += 18
     elif date.hour == 16:
-        print(f"[DEBUG] 16:00 => 16:59 tollfee: 18")
+        print("[DEBUG] 16:00 => 16:59 tollfee: 18")
         period_toll_fee += 18
     elif date.hour == 17:
-        print(f"[DEBUG] 17:00 => 17:59 tollfee: 13")
+        print("[DEBUG] 17:00 => 17:59 tollfee: 13")
         period_toll_fee += 13
     elif date.hour == 18 and date.minute >= 0 and date.minute <= 29:
-        print(f"[DEBUG] 18:00 => 18:29 tollfee: 8")
+        print("[DEBUG] 18:00 => 18:29 tollfee: 8")
         period_toll_fee += 8
     return period_toll_fee
 
 if __name__ == "__main__":
     total_fee = 0
     with open("dataset_car") as file:
-         vehicle_entries = file.readlines()
+        vehicle_entries = file.readlines()
+
+    hour_period_start = datetime.strptime(vehicle_entries[0].strip().split(',')[1], '%Y-%m-%dT%H:%M:%S')
+    print(f"[DEBUG] first hour_period_start: {hour_period_start}")
+    hour_period = hour_period_start + timedelta(hours=1)
+    print(f"[DEBUG] first hour_period: {hour_period}")
 
     last_toll_fee = 0
     for vehicle_entry in vehicle_entries:
         vehicle_entry = vehicle_entry.strip()
         entry_vehicle = vehicle_entry.split(',')[0]
         entry_date = datetime.strptime(vehicle_entry.split(',')[1], '%Y-%m-%dT%H:%M:%S')
-        print(f"vehicle: {entry_vehicle}, time: {entry_date}")
+        print(f"[DEBUG] vehicle: {entry_vehicle}, time: {entry_date}")
         toll_fee = get_toll_fee_for_entry(entry_vehicle, entry_date)
-        print(f"tollfee for entry: {toll_fee}")
-        total_fee += toll_fee
+        print(f"[DEBUG] tollfee for entry: {toll_fee}")
+        if entry_date < hour_period:
+            print("[DEBUG] entry is within hour period")
+            if last_toll_fee < toll_fee:
+                print(f"[DEBUG] toll_fee is higher or equal than last_toll_fee. Remove last_toll_fee ({last_toll_fee}) from total_fee")
+                total_fee -= last_toll_fee
+                print(f"[DEBUG] add {toll_fee} to total_fee")
+                total_fee += toll_fee
+            else:
+                print("[DEBUG] toll_fee is less or equal than last_toll_fee. Do not add fee")
+        else:
+            hour_period = entry_date + timedelta(hours=1)
+            print(f"[DEBUG] no entry is within hour period. New hour period: {hour_period}")
+            print(f"[DEBUG] add {toll_fee} to total_fee")
+            total_fee += toll_fee
+        last_toll_fee = toll_fee
         print("---------------")
 
     if total_fee > 60:
